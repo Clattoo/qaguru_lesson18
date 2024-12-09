4e348a911537e3834ad4f01617da1566de8f9cc8
Maxim Shlemin <clatto@yandex.ru>
2024-12-09 22:29:43 +0300

    Fixed bug

--- src/test/java/tests/DemoTests.java
+++ src/test/java/tests/DemoTests.java
@@ -9,7 +9,7 @@ import models.GetBookListModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import page.ProfilePage;
+import pages.ProfilePage;
 
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
